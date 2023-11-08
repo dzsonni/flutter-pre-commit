3973e6cb43b264ec8849d5b7f8b06c48f2daccd5
Robert Kato <robert.kato@kaladev.com>
2023-11-08 22:55:50 +0200

    Fix

--- hooks/dart_analyze.py
+++ hooks/dart_analyze.py
@@ -2,7 +2,7 @@ import subprocess
 
 def main():
     # Define the command to run
-    command = ['dart', 'format', '.', '--line-length=140']
+    command = ['dart', 'analyze']
 
     # Run the command using subprocess
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
