feb165e8210f2f63fbfae5cd9dcf29b3cc324826
Robert Kato <robert.kato@kaladev.com>
2023-11-08 20:58:47 +0200

    Fix

--- hooks/sort_imports.py
+++ hooks/sort_imports.py
@@ -1,16 +1,16 @@
 import subprocess
 
-# Define the command to run
-command = ['dart', 'run', 'import_sorter:main']
+def main():
+    # Define the command to run
+    command = ['dart', 'run', 'import_sorter:main']
 
-# Run the command using subprocess
-result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # Run the command using subprocess
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-# Print the output and error messages
-print(result.stdout.decode('utf-8'))
-print(result.stderr.decode('utf-8'))
+    # Print the output and error messages
+    print(result.stdout.decode('utf-8'))
+    print(result.stderr.decode('utf-8'))
 
-# Check the return code and exit with an error if it's not 0
-if result.returncode != 0:
-    exit(1)
-import subprocess
+    # Check the return code and exit with an error if it's not 0
+    if result.returncode != 0:
+        exit(1)
