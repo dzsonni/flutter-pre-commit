65927cc65dc14488661d7e6f39f05c61d8d6190e
Robert Kato <robert.kato@kaladev.com>
2023-11-08 23:19:21 +0200

    Fix

--- hooks/sort_imports.py
+++ hooks/sort_imports.py
@@ -8,8 +8,8 @@ def main():
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # Print the output and error messages
-    print(result.stdout.encode('utf-16', errors='ignore'))
-    print(result.stderr.encode('utf-16', errors='ignore'))
+    print(result.stdout.decode('utf-16', errors='ignore'))
+    print(result.stderr.decode('utf-16', errors='ignore'))
 
     # Check the return code and exit with an error if it's not 0
     if result.returncode != 0:
