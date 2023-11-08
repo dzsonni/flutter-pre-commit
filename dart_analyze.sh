518a77809be40a1e93a98bc71360d57fa44501a0
Robert Kato <robert.kato@kaladev.com>
2023-11-08 20:36:20 +0200

    Bash fix

--- dart_analyze.sh
+++ dart_analyze.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # Run Dart analyze and check for errors
 if ! output=$(dart analyze); then
     echo "COMMIT REJECTED: Dart analyze found the following errors:"
--- dart_format.sh
+++ dart_format.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 dart format . --line-length=140
